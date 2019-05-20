c159b047d9e80b3e73f006305e3615e5c4c603e8
Lindo NKAMBULE <lnkambul@c4r16s4.wethinkcode.co.za>
2019-05-20 13:57:27 +0200

    added basic function C files for string operations and memory allocation

---
+++ basic_ext.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   basic_ext.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnkambul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/20 11:21:44 by lnkambul          #+#    #+#             */
+/*   Updated: 2019/05/20 13:21:48 by lnkambul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t		ft_strlen(const char* s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char*		ft_strcpy(char* dst, const char* src)
+{
+	int		i;
+	int		j;
+
+	i = (int)ft_strlen(src);
+	j = (int)ft_strlen(dst);
+	if (i <= j)
+		j = 0;
+		while (j < i)
+		{
+			dst[j] = src[j];
+			j++;
+		}
+}
+
+int		ft_toupper(int	n)
+{
+	if (n > 97 && n < 123)
+		return (n - 32);
+	else
+		return n;
+}
+
+int		ft_tolower(int	n)
+{
+	if (n > 64 && n < 91)
+		return (n + 32);
+	else
+		return n;
+}
+
+
