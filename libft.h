f7127abee69a1f3942c1afd14d5a4265cf1f0f2d
Lindo NKAMBULE <lnkambul@c4r15s1.wethinkcode.co.za>
2019-05-28 15:25:28 +0200

    Tuesday 28th

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lnkambul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 10:03:39 by lnkambul          #+#    #+#             */
-/*   Updated: 2019/05/25 11:41:33 by lnkambul         ###   ########.fr       */
+/*   Updated: 2019/05/28 15:24:48 by lnkambul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,27 @@ int		ft_isdigit(int c);
 
 int		ft_isprint(int c);
 char	*ft_itoa(int n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*memalloc(size_t s);
 
+void	ft_memdel(void **p);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
+
 void	ft_putchar(char c);
 void	ft_putendl(const char *s);
-
 void	ft_putstr(const char *s);
-char	*ft_strcpy(char *dst, const char *src);
+
+char	*ft_strcat(char s1, char s2);
+void	ft_strclr(chari *s);
+char	*ft_strcpy(char *d, const char *s);
+
+void	ft_strdel(char **s);
+char	*ft_strdup(char *s);
+void	ft_striter(char *s, void (*f)(char *));
+
 size_t	ft_strlen(const char *s);
+char	*ft_strmap(const char *s, char (*f)(char *));
+char	*ft_strnew(size_t size);
 
 int		ft_tolower(int c);
 int		ft_toupper(int c);
